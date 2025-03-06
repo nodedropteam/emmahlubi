4f1a4a6585c4772bbc1b669fd6d72ee0e1cd833f
Chilu <chilufya@nodedropp.com>
2025-03-06 11:36:27 -0600

    removing unused variables

--- components/smooth-scroll.tsx
+++ components/smooth-scroll.tsx
@@ -1,11 +1,11 @@
 "use client"
 import React from 'react'
-import { ReactLenis, useLenis } from "lenis/react"
+import { ReactLenis } from "lenis/react"
 
 export default function SmoothScrollProvider({ children }: { children: React.ReactNode }) {
-    const lenis = useLenis(({ scroll }) => {
+//    const lenis = useLenis(({ scroll }) => {
         // lenis instance
-    })
+  //  })
     return (
         <ReactLenis root>
             {children}
